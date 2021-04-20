0d703e16b110d9fd04fdaff39d67b371fc2c70ca
4lexxoranges <76011237+unn4meDd@users.noreply.github.com>
2021-04-20 22:01:46 +0300

    added icons and main menu

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    string str = "https://www.youtube.com/";
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void YouTube()
+    {
+        Application.OpenURL(str);
+    }
+
+    public void Play()
+
+    {
+        SceneManager.LoadScene("Game");
+    }
+    public void Shop()
+    {
+
+    }
+    public void Settings()
+    {
+
+    }
+
+}
